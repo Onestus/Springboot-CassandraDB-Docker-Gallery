e2bda0b017953bf77a3c63fc6c2edaa2d46984af
Onestus <vkryazhevskikh@gmail.com>
2023-05-24 18:12:15 +0300

    Sorting by id doesn't work :(

--- src/main/java/com/example/cassandra/springbootcassandragallery/model/Painting.java
+++ src/main/java/com/example/cassandra/springbootcassandragallery/model/Painting.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.cassandra.core.cql.Ordering;
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 import org.springframework.data.cassandra.core.mapping.Table;
@@ -19,7 +20,13 @@ public class Painting {
     @PrimaryKey
     @PrimaryKeyColumn(name = "id", ordinal = 0, type = PrimaryKeyType.CLUSTERED, ordering = Ordering.ASCENDING)
     private int id;
+
+    @Column("name")
     private String name;
+
+    @Column("year")
     private int year;
+
+    @Column("description")
     private String description;
 }
